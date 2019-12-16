d83d4eea29a8dfedc2c7d6ccc46848edcebe352a
Shashank <shashank136.sk@gmail.com>
2019-12-16 11:41:54 +0530

    Added first custom React component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent.js';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Shashank Kumar</NavbarBrand>
         </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
